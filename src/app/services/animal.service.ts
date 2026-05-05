2ceffbca52a358097ece709bd197820b13470d52
Łukasz <skotniczny@outlook.com>
2026-05-05 13:38:22 +0200

    fix: configure deployment for GitHub Pages

--- src/app/services/animal.service.ts
+++ src/app/services/animal.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable, inject } from '@angular/core';
 import { Observable } from "rxjs"
 import { Animal, AnimalsListItem, AnimalClasses } from "../models/animal";
+import { PlatformLocation } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
@@ -9,18 +10,17 @@ import { Animal, AnimalsListItem, AnimalClasses } from "../models/animal";
 
 export class AnimalService {
   #http = inject(HttpClient);
+  #base = inject(PlatformLocation).getBaseHrefFromDOM();
 
   getAnimals(): Observable<AnimalsListItem[]> {
-    return this.#http.get<AnimalsListItem[]>("/animals/animals.json");
+    return this.#http.get<AnimalsListItem[]>(`${this.#base}animals/animals.json`);
   }
 
   getClasses(): Observable<AnimalClasses> {
-    return this.#http.get<AnimalClasses>("/animals/classes.json");
+    return this.#http.get<AnimalClasses>(`${this.#base}animals/classes.json`);
   }
 
   getAnimal(id: string): Observable<Animal> {
-    return this.#http.get<Animal>(`/animals/${id}.json`);
+    return this.#http.get<Animal>(`${this.#base}animals/${id}.json`);
   }
 }
-
-
